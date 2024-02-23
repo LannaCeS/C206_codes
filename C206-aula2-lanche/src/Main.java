560d883209536fadbce8559e8ebd907d7984c2a9
LannaCeS <152916507+LannaCeS@users.noreply.github.com>
2024-02-23 14:18:47 -0300

    Exercicio_1- lanche

---
+++ C206-aula2-lanche/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int sum = (lanche1+lanche2+lanche3);
+        float media = sum/3.0f;
+
+        System.out.println("Total: "+(lanche1+lanche2+lanche3));
+        System.out.println("Media: "+(media));
+    }
+}
