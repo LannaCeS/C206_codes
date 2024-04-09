9168b7ad0720cef53ea4ca43f8525116e619dc55
LannaCeS <152916507+LannaCeS@users.noreply.github.com>
2024-04-09 13:47:15 -0300

    Add files via upload
    organização em pacotes

---
+++ example_pacotes/src/Main.java
@@ -0,0 +1,8 @@
+import com.org.example.Math; //referencing to the first Math's package
+public class Main {
+    public static void main(String[] args) {
+
+        Math math = new Math();
+        com.org.exampleII.Math ma = new com.org.exampleII.Math(); //fully qualified name
+    }
+}
---
+++ example_pacotes/src/com/org/exampleII/Math.java
@@ -0,0 +1,4 @@
+package com.org.exampleII;
+
+public class Math {
+}
